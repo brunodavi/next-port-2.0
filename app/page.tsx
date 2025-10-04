17da5c769df12bf58441511ff63a1340837a356d
brunodavi <48166556+brunodavi@users.noreply.github.com>
2025-10-04 18:46:05 -0300

    feat: reorganiza a estrutura do componente Home e aplica estilos ao perfil

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,28 @@
 import Image from "next/image";
-import { GITHUB_PROFILE_IMAGE_URL, SITE_DESCRIPTION, SITE_NAME } from "./lib/constants";
+import { GITHUB_PROFILE_IMAGE_URL } from "./lib/constants";
+import ProfileMarkdown from "./components/ProfleMarkdown";
+import ThemeToggle from "./components/ThemeToggle";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold">Welcome to {SITE_NAME}</h1>
-      <p className="mt-4">{SITE_DESCRIPTION}</p>
-      <Image
-        src={GITHUB_PROFILE_IMAGE_URL}
-        alt="Profile Picture"
-        width={150}
-        height={150}
-        className="rounded-full mt-4"
-      />
+    <div className="min-h-screen transition-colors">
+      <div className="container mx-auto p-4">
+        <div className="flex justify-end mb-4">
+          <ThemeToggle />
+        </div>
+        <div className="flex flex-col items-center">
+          <Image
+            src={GITHUB_PROFILE_IMAGE_URL}
+            alt="Profile Picture"
+            width={150}
+            height={150}
+            className="rounded-full mt-4 border-4 border-gray-300 dark:border-gray-700"
+          />
+        </div>
+        <div className="max-w-4xl mx-auto mt-8">
+          <ProfileMarkdown />
+        </div>
+      </div>
     </div>
   );
 }
