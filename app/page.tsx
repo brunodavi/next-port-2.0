1e2a6baa2bb87e7cb4907895a7dd7980076ee7ef
brunodavi <48166556+brunodavi@users.noreply.github.com>
2025-10-04 23:47:18 -0300

    feat: remove imagem de perfil do componente Home

--- app/page.tsx
+++ app/page.tsx
@@ -10,15 +10,6 @@ export default function Home() {
         <div className="flex justify-end mb-4">
           <ThemeToggle />
         </div>
-        <div className="flex flex-col items-center">
-          <Image
-            src={GITHUB_PROFILE_IMAGE_URL}
-            alt="Profile Picture"
-            width={150}
-            height={150}
-            className="rounded-full mt-4 border-4 border-gray-300 dark:border-gray-700"
-          />
-        </div>
         <div className="max-w-4xl mx-auto mt-8">
           <ProfileMarkdown />
         </div>
